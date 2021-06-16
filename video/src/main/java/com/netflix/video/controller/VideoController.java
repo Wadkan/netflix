53f4c3cc4ef9367df842c83a3ed15e7c10fdf3cd
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-06-16 16:40:42 +0200

    get one video with recommendation + swagger works

---
+++ video/src/main/java/com/netflix/video/controller/VideoController.java
@@ -0,0 +1,88 @@
+package com.netflix.video.controller;
+
+import com.netflix.video.entity.Video;
+import com.netflix.video.repository.VideoRepository;
+import com.netflix.video.service.RecommendationCaller;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@Slf4j
+@RequestMapping("/video")
+public class VideoController {
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    RecommendationCaller recommendationCaller;
+
+
+    @GetMapping("/{id}")
+    public OneVideo getVideoById(@PathVariable("id") Long id) {
+
+        Optional<Video> video = videoRepository.findById(id);
+        video.get().setRecommendation(recommendationCaller.getRecommendations(video.get().getId()));
+
+        return new OneVideo(
+                environment.getProperty("server.port"),
+                video
+        );
+    }
+
+    @GetMapping("/all")
+    public Videos getAllVideos() {
+        return new Videos(
+                environment.getProperty("server.port"),
+                videoRepository.findAll()
+        );
+    }
+
+    @GetMapping("/all-with-recommend")
+    private Videos getAllVideosWithRecommendtations() {
+
+        List<Video> videosWithRecommendation = videoRepository.findAll();
+
+        videosWithRecommendation.stream().
+                forEach(
+                        video -> video.setRecommendation(recommendationCaller.getRecommendations(video.getId()))
+                );
+
+
+        Videos videos = new Videos(
+                environment.getProperty("server.port"),
+                videosWithRecommendation
+        );
+
+        return videos;
+    }
+
+
+    @Data
+    @AllArgsConstructor
+    public class Videos {
+        private String port;
+        private List<Video> videos;
+    }
+
+    @Data
+    @AllArgsConstructor
+    public class OneVideo {
+        private String port;
+        private Optional<Video> video;
+    }
+
+}

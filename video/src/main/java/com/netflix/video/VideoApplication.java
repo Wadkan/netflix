0f6f0597e070515491dd89d4f2e8e3c65e3d7c99
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-06-16 11:55:32 +0200

    getAllVideos API works with swagger

--- video/src/main/java/com/netflix/video/VideoApplication.java
+++ video/src/main/java/com/netflix/video/VideoApplication.java
@@ -9,7 +9,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDate;
 import java.util.Arrays;
 

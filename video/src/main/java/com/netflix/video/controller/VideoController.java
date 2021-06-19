3f33fd6ba6902305398cf31757136f22f3e09a00
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-06-19 11:27:18 +0200

    put /video/{id} into service package

--- video/src/main/java/com/netflix/video/controller/VideoController.java
+++ video/src/main/java/com/netflix/video/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.netflix.video.controller;
 import com.netflix.video.entity.Video;
 import com.netflix.video.repository.VideoRepository;
 import com.netflix.video.service.RecommendationCaller;
+import com.netflix.video.service.VideoService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -20,62 +21,57 @@ import java.util.Optional;
 public class VideoController {
 
     @Autowired
-    private VideoRepository videoRepository;
-
-    @Autowired
     private Environment environment;
 
     @Autowired
-    RecommendationCaller recommendationCaller;
+    VideoService videoService;
+
 
     @GetMapping("/{id}")
     public OneVideo getVideoById(@PathVariable("id") Long id) {
 
-        Optional<Video> video = videoRepository.findById(id);
-        video.get().setRecommendation(recommendationCaller.getRecommendations(video.get().getId()));
-
         return new OneVideo(
                 environment.getProperty("server.port"),
-                video
+                videoService.getVideoWithRecommendation(id)
         );
     }
 
-    @PutMapping("/{id}")
-    public OneVideo updateVideoById(@RequestBody Video updatedVideo) {
-        videoRepository.save(updatedVideo);
-
-        return new OneVideo(
-                environment.getProperty("server.port"),
-                Optional.of(updatedVideo)
-        );
-    }
-
-    @GetMapping("/all")
-    public Videos getAllVideos() {
-        return new Videos(
-                environment.getProperty("server.port"),
-                videoRepository.findAll()
-        );
-    }
-
-    @GetMapping("/all-with-recommend")
-    private Videos getAllVideosWithRecommendtations() {
-
-        List<Video> videosWithRecommendation = videoRepository.findAll();
-
-        videosWithRecommendation.stream().
-                forEach(
-                        video -> video.setRecommendation(recommendationCaller.getRecommendations(video.getId()))
-                );
-
-
-        Videos videos = new Videos(
-                environment.getProperty("server.port"),
-                videosWithRecommendation
-        );
-
-        return videos;
-    }
+//    @PutMapping("/{id}")
+//    public OneVideo updateVideoById(@RequestBody Video updatedVideo) {
+//        videoRepository.save(updatedVideo);
+//
+//        return new OneVideo(
+//                environment.getProperty("server.port"),
+//                Optional.of(updatedVideo)
+//        );
+//    }
+//
+//    @GetMapping("/all")
+//    public Videos getAllVideos() {
+//        return new Videos(
+//                environment.getProperty("server.port"),
+//                videoRepository.findAll()
+//        );
+//    }
+//
+//    @GetMapping("/all-with-recommend")
+//    private Videos getAllVideosWithRecommendtations() {
+//
+//        List<Video> videosWithRecommendation = videoRepository.findAll();
+//
+//        videosWithRecommendation.stream().
+//                forEach(
+//                        video -> video.setRecommendation(recommendationCaller.getRecommendations(video.getId()))
+//                );
+//
+//
+//        Videos videos = new Videos(
+//                environment.getProperty("server.port"),
+//                videosWithRecommendation
+//        );
+//
+//        return videos;
+//    }
 
 
     @Data
--- video/src/main/java/com/netflix/video/service/VideoService.java
+++ video/src/main/java/com/netflix/video/service/VideoService.java
@@ -1,4 +1,24 @@
 package com.netflix.video.service;
 
+import com.netflix.video.entity.Video;
+import com.netflix.video.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class VideoService {
+
+    @Autowired
+    private VideoRepository videoRepository;
+
+    @Autowired
+    RecommendationCaller recommendationCaller;
+
+    public Optional<Video> getVideoWithRecommendation(Long id) {
+        Optional<Video> video = videoRepository.findById(id);
+        video.get().setRecommendation(recommendationCaller.getRecommendations(video.get().getId()));
+        return video;
+    }
 }

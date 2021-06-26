7bbfd1836e7a25bb988e5affe8c6a5f37ec5d58f
Wadkan <60811289+Wadkan@users.noreply.github.com>
2021-06-26 23:01:28 +0200

    .gitignore fixed

---
+++ netflix/src/main/java/com/netflix/netflix/model/VideoIncomingData.java
@@ -0,0 +1,12 @@
+package com.netflix.netflix.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class VideoIncomingData {
+    private in
+
+    // 3. video: 23:00
+}
